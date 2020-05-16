52b3423612f1df77bbc5bcddf04c96eb2df3f9ec
VuDaiDuong-c0220h1 <61004399+VuDaiDuong-c0220h1@users.noreply.github.com>
2020-05-17 00:36:45 +0700

    Add files via upload

---
+++ database/duongshoe_catalog.sql
@@ -0,0 +1,6 @@
+INSERT INTO duongshoe.catalog (id, catalog_code, name, description, status, create_date, update_date) VALUES (1, 1, 'ADIDAS', 'Các sản phẩm đến từ hãng Adidas', 1, '2020-05-16 14:24:26', '2020-05-16 14:24:26');
+INSERT INTO duongshoe.catalog (id, catalog_code, name, description, status, create_date, update_date) VALUES (2, 2, 'NIKE', 'Các sản phẩm đến từ hãng Nike', 1, '2020-05-16 14:24:26', '2020-05-16 14:24:26');
+INSERT INTO duongshoe.catalog (id, catalog_code, name, description, status, create_date, update_date) VALUES (3, 3, 'GUCCI', 'Các sản phẩm đến từ hãng Gucci', 1, '2020-05-16 14:24:26', '2020-05-16 14:24:26');
+INSERT INTO duongshoe.catalog (id, catalog_code, name, description, status, create_date, update_date) VALUES (4, 4, 'PUMA', 'Các sản phẩm đến từ hãng Puma', 1, '2020-05-16 14:24:26', '2020-05-16 14:24:26');
+INSERT INTO duongshoe.catalog (id, catalog_code, name, description, status, create_date, update_date) VALUES (5, 5, 'BITIS', 'Các sản phẩm đến từ hãng Biti''s', 1, '2020-05-16 14:24:26', '2020-05-16 14:24:26');
+INSERT INTO duongshoe.catalog (id, catalog_code, name, description, status, create_date, update_date) VALUES (6, 6, 'VANS', 'Các sản phẩm đến từ hãng Vans', 0, '2020-05-16 14:24:26', '2020-05-16 14:24:26');
